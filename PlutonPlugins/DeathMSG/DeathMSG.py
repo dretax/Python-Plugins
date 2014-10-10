a5e32e2bbd257920ef0f8f014aa678fae3f67472
DreTaX <dretax14@gmail.com>
2014-10-10 19:14:46 +0200

    Fixes

--- PlutonPlugins/DeathMSG/DeathMSG.py
+++ PlutonPlugins/DeathMSG/DeathMSG.py
@@ -24,28 +24,29 @@ class DeathMSG:
         return Plugin.GetIni("TpFriendConfig")
 
     def On_PlayerDied(self, PlayerDeathEvent):
-        Server.BroadcastFrom("DeathMSG", str(PlayerDeathEvent.Attacker.Name + " | " + PlayerDeathEvent.Victim.Name + " | " + PlayerDeathEvent.DamageType  + " | " + PlayerDeathEvent.DamageAmount))
-        if PlayerDeathEvent.Attacker is not None and PlayerDeathEvent.Victim is not None and PlayerDeathEvent.Attacker != PlayerDeathEvent.Victim:
-            ini = self.DeathMSGConfig()
-            sysname = ini.GetSetting("Settings", "SysName")
-            dmgmsg = ini.GetSetting("Settings", "KillMessage")
-            attacker = PlayerDeathEvent.Attacker.ToPlayer()
-            victim = PlayerDeathEvent.Victim
-            vloc = victim.Location
-            aloc = attacker.Location
-            dist = round(Util.GetVectorsDistance(vloc, aloc), 2)
-            damage = PlayerDeathEvent.DamageAmount
-            dmgmsg.replace("killer", attacker.Name)
-            dmgmsg.replace("victim", victim.Name)
-            dmgmsg.replace("dmg", str(damage))
-            dmgmsg.replace("dist", str(dist))
-            # = dmgmsg.replace("weapon", WEAPONMISSINGOMG)
-            Server.BroadcastFrom(sysname, dmgmsg)
-
-        if PlayerDeathEvent.Attacker is not None and PlayerDeathEvent.Victim is not None and PlayerDeathEvent.Attacker == PlayerDeathEvent.Victim:
-            ini = self.DeathMSGConfig()
-            suicidemsg = int(ini.GetSetting("Settings", "SuicideMsgs"))
-            sysname = ini.GetSetting("Settings", "SysName")
-            if suicidemsg == 1:
+        attacker = PlayerDeathEvent.Attacker.ToPlayer()
+        if attacker is not None:
+            Server.BroadcastFrom("DeathMSG", str(attacker.Name + " | " + PlayerDeathEvent.Victim.Name + " | " + PlayerDeathEvent.DamageType  + " | " + PlayerDeathEvent.DamageAmount))
+            if PlayerDeathEvent.Victim is not None and attacker != PlayerDeathEvent.Victim:
+                ini = self.DeathMSGConfig()
+                sysname = ini.GetSetting("Settings", "SysName")
+                dmgmsg = ini.GetSetting("Settings", "KillMessage")
                 victim = PlayerDeathEvent.Victim
-                Server.BroadcastFrom(sysname, victim.Name + " suicided...")
+                vloc = victim.Location
+                aloc = attacker.Location
+                dist = round(Util.GetVectorsDistance(vloc, aloc), 2)
+                damage = PlayerDeathEvent.DamageAmount
+                dmgmsg.replace("killer", attacker.Name)
+                dmgmsg.replace("victim", victim.Name)
+                dmgmsg.replace("dmg", str(damage))
+                dmgmsg.replace("dist", str(dist))
+                # = dmgmsg.replace("weapon", WEAPONMISSINGOMG)
+                Server.BroadcastFrom(sysname, dmgmsg)
+
+            if PlayerDeathEvent.Attacker is not None and PlayerDeathEvent.Victim is not None and PlayerDeathEvent.Attacker == PlayerDeathEvent.Victim:
+                ini = self.DeathMSGConfig()
+                suicidemsg = int(ini.GetSetting("Settings", "SuicideMsgs"))
+                sysname = ini.GetSetting("Settings", "SysName")
+                if suicidemsg == 1:
+                    victim = PlayerDeathEvent.Victim
+                    Server.BroadcastFrom(sysname, victim.Name + " suicided...")
