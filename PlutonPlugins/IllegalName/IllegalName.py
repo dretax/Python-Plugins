ce4b160dd74348c78269a4b213d1fa8c14ce4bce
DreTaX <dretax14@gmail.com>
2014-10-16 20:00:21 +0200

    IllegalName

---
+++ PlutonPlugins/IllegalName/IllegalName.py
@@ -0,0 +1,76 @@
+__author__ = 'DreTaX'
+__version__ = '1.0'
+
+import clr
+
+clr.AddReferenceByPartialName("Pluton")
+import Pluton
+import re
+
+"""
+    Class
+"""
+
+
+class IllegalName:
+
+    def getIllegal(self):
+        if not Plugin.IniExists("IllegalNames"):
+            IllegalNames = Plugin.CreateIni("IllegalNames")
+            IllegalNames.AddSetting("IllegalNames", "Name1", "Suck")
+            IllegalNames.AddSetting("IllegalNames", "Name2", "Fuck")
+            IllegalNames.AddSetting("IllegalNames", "Name3", "SHITSERVER")
+            IllegalNames.Save()
+        return Plugin.GetIni("IllegalNames")
+
+    def IllegalNameConfig(self):
+        if not Plugin.IniExists("IllegalNameConfig"):
+            loc = Plugin.CreateIni("IllegalNameConfig")
+            loc.Save()
+        return Plugin.GetIni("IllegalNameConfig")
+
+
+    def On_PlayerConnected(self, Player):
+        name = str(Player.Name)
+        n = len(name)
+        ini = self.IllegalNameConfig()
+        illini = self.getIllegal()
+        f = int(ini.GetSetting("options", "protection1"))
+        reason = ini.GetSetting("options", "DisconnectReason")
+        reason2 = ini.GetSetting("options", "DisconnectReason2")
+        reason3 = ini.GetSetting("options", "DisconnectReason3")
+        space = ini.GetSetting("options", "Spaces")
+        listnames = illini.EnumSection("IllegalNames")
+        counted = len(listnames)
+        i = 0
+        if counted > 0:
+            for checkn in listnames:
+                get = illini.GetSetting("IllegalNames", checkn)
+                i += 1
+                lowername = name.lower()
+                lowercheck = get.lower()
+                if counted >= i:
+                    if lowercheck in lowername:
+                        Player.Kick(reason)
+                        return
+        if f == 1:
+            if space == 0:
+                a = re.match('^[a-zA-Z0-9_!+?%éáűőúöüó()<>/\@#,.\[\]-]+$', name)
+                if not a:
+                    Player.Kick(reason3)
+            else:
+                a = re.match('^[a-zA-Z0-9_ !+?%éáűőúöüó()<>/\@#,.\[\]-]+$', name)
+                if not a or n <= 1:
+                    Player.Kick(reason2)
+        elif f == 2:
+            if n <= 1:
+                Player.Kick(reason2)
+        elif f == 3:
+            if (space == 0):
+                a = re.match('^[a-zA-Z0-9_!+?%éáűőúöüó()<>/\@#,.\[\]-]+$', name)
+                if not a or n <= 1:
+                    Player.Kick(reason3)
+            else:
+                a = re.match('^[a-zA-Z0-9_ !+?%éáűőúöüó()<>/\@#,.\[\]-]+$', name)
+                if not a or n <= 1:
+                    Player.Kick(reason2)
