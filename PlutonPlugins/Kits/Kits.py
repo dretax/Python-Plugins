068fa569b2dcf3211396b833f69b3b422f63a20c
DreTaX <dretax14@gmail.com>
2014-10-20 13:05:07 +0200

    Format

--- PlutonPlugins/Kits/Kits.py
+++ PlutonPlugins/Kits/Kits.py
@@ -25,49 +25,49 @@ class Kits:
         return Plugin.GetIni("KitsConfig")
 
     def On_Command(self, cmd):
-            Player = cmd.User
-            args = cmd.args
-            if cmd.cmd == "kit":
-                ini = self.KitsConfig()
-                if Player.Admin:
-                    akits = ini.GetSetting("AdminKits", "AvailableKits")
-                    if len(args) == 0:
-                        Player.MessageFrom("Kits", "Available Kits: " + akits)
-                        return
-                    if Server.LoadOuts.ContainsKey(args[0]):
-                        loadout = Server.LoadOuts[args[0]]
-                        loadout.ToInv(Player.Inventory)
-                        return
-                    else:
-                        Player.Message("Kit " + str(args[0]) + " not found!")
-                        return
+        Player = cmd.User
+        args = cmd.args
+        if cmd.cmd == "kit":
+            ini = self.KitsConfig()
+            if Player.Admin:
+                akits = ini.GetSetting("AdminKits", "AvailableKits")
+                if len(args) == 0:
+                    Player.MessageFrom("Kits", "Available Kits: " + akits)
+                    return
+                if Server.LoadOuts.ContainsKey(args[0]):
+                    loadout = Server.LoadOuts[args[0]]
+                    loadout.ToInv(Player.Inventory)
+                    return
                 else:
-                    pkits = ini.GetSetting("PlayerKits", "AvailableKits")
-                    if len(args) == 0:
-                        Player.MessageFrom("Kits", "Available Kits: " + pkits)
-                        return
-                    cooldown = int(ini.GetSetting("PlayerKits", "Cooldown"))
-                    if cooldown > 0:
-                        systick = System.Environment.TickCount
-                        time = DataStore.Get("startercooldown", Player.SteamID)
-                        if time is None or (systick - time) < 0 or math.isnan(systick - time):
-                            DataStore.Add("startercooldown", Player.SteamID, 7)
-                        calc = systick - time
-                        if calc >= cooldown or time == 7:
-                            if Server.LoadOuts.ContainsKey(args[0]):
-                                loadout = Server.LoadOuts[args[0]]
-                                loadout.ToInv(Player.Inventory)
-                                DataStore.Add("startercooldown", Player.SteamID, System.Environment.TickCount)
-                            else:
-                                Player.Message("Kit " + str(args[0]) + " not found!")
-                        else:
-                            Player.Message("You have to wait before using this again!")
-                            done = round((calc / 1000) / 60, 2)
-                            done2 = round((cooldown / 1000) / 60, 2)
-                            Player.Message("Time Remaining: " + str(done) + "/" + str(done2) + " minutes")
-                    else:
+                    Player.Message("Kit " + str(args[0]) + " not found!")
+                    return
+            else:
+                pkits = ini.GetSetting("PlayerKits", "AvailableKits")
+                if len(args) == 0:
+                    Player.MessageFrom("Kits", "Available Kits: " + pkits)
+                    return
+                cooldown = int(ini.GetSetting("PlayerKits", "Cooldown"))
+                if cooldown > 0:
+                    systick = System.Environment.TickCount
+                    time = DataStore.Get("startercooldown", Player.SteamID)
+                    if time is None or (systick - time) < 0 or math.isnan(systick - time):
+                        DataStore.Add("startercooldown", Player.SteamID, 7)
+                    calc = systick - time
+                    if calc >= cooldown or time == 7:
                         if Server.LoadOuts.ContainsKey(args[0]):
                             loadout = Server.LoadOuts[args[0]]
                             loadout.ToInv(Player.Inventory)
+                            DataStore.Add("startercooldown", Player.SteamID, System.Environment.TickCount)
                         else:
                             Player.Message("Kit " + str(args[0]) + " not found!")
+                    else:
+                        Player.Message("You have to wait before using this again!")
+                        done = round((calc / 1000) / 60, 2)
+                        done2 = round((cooldown / 1000) / 60, 2)
+                        Player.Message("Time Remaining: " + str(done) + "/" + str(done2) + " minutes")
+                else:
+                    if Server.LoadOuts.ContainsKey(args[0]):
+                        loadout = Server.LoadOuts[args[0]]
+                        loadout.ToInv(Player.Inventory)
+                    else:
+                        Player.Message("Kit " + str(args[0]) + " not found!")
