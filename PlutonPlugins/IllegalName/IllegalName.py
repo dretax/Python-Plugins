e6a23577ef359c17ea58e0a4e10b5f342c80bf41
DreTaX <dretax14@gmail.com>
2014-10-25 19:05:26 +0200

    IllegalName

--- PlutonPlugins/IllegalName/IllegalName.py
+++ PlutonPlugins/IllegalName/IllegalName.py
@@ -30,26 +30,21 @@ class IllegalName:
         return Plugin.GetIni("IllegalNameConfig")
 
     def CutName(self, string):
-        try:
-            name = string.encode('UTF-8')
-            name = name.decode('UTF-8', 'strict')
-            name = re.sub(r'[^\x00-\x7F]+','', name)
-            return name
-        except:
-            return False
+        name = string.encode('UTF-8')
+        name = name.decode('UTF-8', 'strict')
+        name = re.sub(r'[^\x00-\x7F]+','', name)
+        return name
 
     def On_ClientAuth(self, AuthEvent):
         Player = AuthEvent.con
         name = str(AuthEvent.Name)
         n = len(name)
         ini = self.IllegalNameConfig()
-        asciie = int(ini.GetSetting("options", "CheckForAscii"))
+        asciie = int(ini.GetSetting("options", "CheckForNonAscii"))
+        regex = int(ini.GetSetting("options", "CheckWithRegEx"))
         illini = self.getIllegal()
-        f = int(ini.GetSetting("options", "protection1"))
         reason = ini.GetSetting("options", "DisconnectReason")
         reason2 = ini.GetSetting("options", "DisconnectReason2")
-        reason3 = ini.GetSetting("options", "DisconnectReason3")
-        space = ini.GetSetting("options", "Spaces")
         listnames = illini.EnumSection("IllegalNames")
         counted = len(listnames)
         i = 0
@@ -63,39 +58,19 @@ class IllegalName:
                     if lowercheck in lowername:
                         Player.Kick(reason)
                         return
-        if f == 1:
-            if space == 0:
-                a = re.match('^[a-zA-Z0-9_!+?%éáűőúöüó()<>/\@#,.\[\]-]+$', name)
-                if not a:
-                    Player.Kick(reason3)
-                    return
-            else:
-                a = re.match('^[a-zA-Z0-9_!+?%éáűőúöüó()<>/\@#,.\\s\[\]-]+$', name)
-                if not a or n <= 1:
-                    Player.Kick(reason2)
-                    return
-                AuthEvent.con.username = re.sub(' +',' ', name)
-                name = re.sub(' +',' ', name)
-        elif f == 2:
-            if n <= 1:
+        if regex == 1:
+            starts = name.startswith(' ')
+            ends = name.endswith(' ')
+            if starts is True:
+                name.replace(name[0], '')
+            if ends is True:
+                name.replace(name[n], '')
+            a = re.match('^[a-zA-Z0-9_!+?%éáűőúöüó()<>/\@#,.\\s\[\]-]+$', name)
+            if not a or n <= 1:
                 Player.Kick(reason2)
-        elif f == 3:
-            if space == 0:
-                a = re.match('^[a-zA-Z0-9_!+?%éáűőúöüó()<>/\@#,.\[\]-]+$', name)
-                if not a or n <= 1:
-                    Player.Kick(reason3)
-                    return
-            else:
-                a = re.match('^[a-zA-Z0-9_!+?%éáűőúöüó()<>/\@#,.\\s\[\]-]+$', name)
-                if not a or n <= 1:
-                    Player.Kick(reason2)
-                    return
-                AuthEvent.con.username = re.sub(' +',' ', name)
-                name = re.sub(' +',' ', name)
-        if asciie == 1:
-            asciireason = ini.GetSetting("options", "AsciiReason")
-            newname = self.CutName(name)
-            if newname is False:
-                Player.Kick(asciireason)
                 return
-            AuthEvent.con.username = newname
+            name = re.sub(' +',' ', name)
+        if asciie == 1:
+            newname = self.CutName(name)
+            name = newname
+        AuthEvent.con.username = name
