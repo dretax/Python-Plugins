e490d8dcca9b01fd7cc9147fdbcac760d37b51b7
DreTaX <dretax14@gmail.com>
2014-10-27 16:00:20 +0100

    Library

--- Pluton/Entity.py
+++ Pluton/Entity.py
@@ -4,7 +4,7 @@ __version__ = '1.0'
 """
     This file was created for plugin developers to be able to use the correct functions
     without looking at the wiki or the api.
-    This file is for API showoff only, and nothing else.
+    API showoff purposes only, and nothing else.
 """
 
 class Entity:
