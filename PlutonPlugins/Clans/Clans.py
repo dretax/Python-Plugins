a0022ae7914ebdaff070f38ff51c230b819db209
DreTaX <dretax14@gmail.com>
2014-11-30 22:40:38 +0100

    Clan Commands

--- PlutonPlugins/Clans/Clans.py
+++ PlutonPlugins/Clans/Clans.py
@@ -8,7 +8,7 @@ import Pluton
 import System
 from System import String
 
-class TClans:
+class Clans:
 
     """
         Clan Methods.
@@ -59,7 +59,7 @@ class TClans:
         for id in sec:
             ids.append(id)
         for player in Server.ActivePlayers:
-            if player.SteamID in ids:
+            if player.GameID in ids:
                 Members.append(player)
         return Members
 
@@ -162,6 +162,35 @@ class TClans:
             s = s + m +", "
         return s
 
+    def IsPending(self, id):
+        if DataStore.ContainsKey("Clans", id):
+            return True
+        return False
+
+    def SendPrivateMessage(self, Clan, FromPlayer, Message):
+        online = self.GetAllOnlinePlayersOfClan(Clan)
+        for pl in online:
+            if pl is not None:
+                pl.MessageFrom(Clan, FromPlayer + " -> " + Message)
+
+    def MakePending(self, id, idinviter):
+        DataStore.Add("Clans", id, idinviter)
+
+    def ClansAutoKillCallback(self, timer):
+        timer.Kill()
+        ini = self.ClansConfig()
+        sys = ini.GetSetting("Settings", "Sys")
+        autokill = timer.Args
+        if not self.IsPending(autokill["PlayerT"]):
+            return
+        DataStore.Remove("Clans", autokill["PlayerT"])
+        PlayerFrom = Server.FindPlayer(autokill["PlayerR"])
+        PlayerP = Server.FindPlayer(autokill["PlayerT"])
+        if PlayerFrom is None or PlayerP is None:
+            return
+        PlayerFrom.MessageFrom(sys, "Request timed out.")
+        PlayerP.MessageFrom(sys, "Request timed out.")
+
     def GetPlayerName(self, namee):
         name = namee.lower()
         for pl in Server.ActivePlayers:
@@ -213,17 +242,99 @@ class TClans:
     """
 
     def On_PlayerConnected(self, Player):
-        if self.HasClan(Player.SteamID):
-            clan = self.GetClanOfPlayer(Player.SteamID)
+        if self.HasClan(Player.GameID):
+            clan = self.GetClanOfPlayer(Player.GameID)
             name = Player.Name
             Player.basePlayer.displayName = "[" + clan + "] " + name
 
     def On_Command(self, cmd):
         Player = cmd.User
-        args = cmd.args
+        args = cmd.quotedArgs
         command = cmd.cmd
         cfg = self.ClansConfig()
         sys = cfg.GetSetting("Settings", "Sys")
         if command == "chelp":
             Player.MessageFrom(sys, "TClans Created by " + __author__ + " V" + __version__)
             #todo: ......
+        elif command == "ccreate":
+            if len(args) == 0:
+                Player.MessageFrom(sys, "Usage /ccreate clanname")
+                return
+            id = Player.GameID
+            if self.HasClan(id):
+                Player.MessageFrom(sys, "You already have a clan. Leave first.")
+                return
+            self.CreateClan(args, id, str(Player.Name))
+            Server.BroadcastFrom(sys, args + " got created by " + Player.Name)
+            Player.MessageFrom(sys, "You created your first clan! /cinvite playername to invite!")
+        elif command == "cinvite":
+            if len(args) == 0:
+                Player.MessageFrom(sys, "Usage /cinvite playername")
+                return
+            id = Player.GameID
+            if not self.HasClan(id):
+                Player.MessageFrom(sys, "You don't have a clan!")
+                return
+            if self.GetClanRank(id) == 1:
+                Player.MessageFrom(sys, "You must be an officer or an owner to do this.")
+                return
+            playerr = self.CheckV(Player, args)
+            if playerr is None:
+                return
+            if self.HasClan(playerr.GameID):
+                Player.MessageFrom(sys, "This player has a clan.")
+                return
+            if self.IsPending(playerr.GameID):
+                Player.MessageFrom(sys, "This player is pending a request. Try after a few seconds.")
+                return
+            clan = self.GetClanOfPlayer(id)
+            self.MakePending(playerr.GameID, id)
+            playerr.MessageFrom(sys, "Clan " + clan + " invited you to join their forces!")
+            playerr.MessageFrom(sys, "Type /cjoin to accept or /cdeny to deny! You have 40 seconds to accept.")
+            autokill = Plugin.CreateDict()
+            autokill["PlayerR"] = Player.GameID
+            autokill["PlayerT"] = playerr.GameID
+            Plugin.CreateParallelTimer("ClansAutoKill", 1000 * 40, autokill).Start()
+        elif command == "cdeny":
+            if len(args) == 0:
+                Player.MessageFrom(sys, "Usage /cdeny")
+                return
+            id = Player.GameID
+            if not self.IsPending(id):
+                Player.MessageFrom(sys, "You aren't pending any requests!")
+                return
+            inviterid = DataStore.Get("Clans", id)
+            inviter = Server.FindPlayer(inviterid)
+            DataStore.Remove("Clans", id)
+            inviter.MessageFrom(sys, Player.Name + "  Denied the request!")
+            Player.MessageFrom(sys, "You denied the request!")
+        elif command == "cjoin":
+            if len(args) == 0:
+                Player.MessageFrom(sys, "Usage /cjoin")
+                return
+            id = Player.GameID
+            if self.HasClan(id):
+                Player.MessageFrom(sys, "You already have a clan!")
+                return
+            if not self.IsPending(id):
+                Player.MessageFrom(sys, "You aren't pending any requests!")
+                return
+            inv = DataStore.Get("Clans", id)
+            clan = self.GetClanOfPlayer(inv)
+            self.AddPlayerToClan(clan, id, str(Player.Name))
+            online = self.GetAllOnlinePlayersOfClan(clan)
+            for pl in online:
+                if pl is not None:
+                    pl.MessageFrom(sys, Player.Name + " joined to the clan!")
+        elif command == "cm":
+            if len(args) == 0:
+                Player.MessageFrom(sys, "Usage /cm message")
+                return
+            name = str(Player.Name)
+            id = Player.GameID
+            if not self.HasClan(id):
+                Player.MessageFrom(sys, "You don't have a clan!")
+                return
+            clan = self.GetClanOfPlayer(id)
+            self.SendPrivateMessage(clan, name, args)
+        #Todo: Finish the other commands.
