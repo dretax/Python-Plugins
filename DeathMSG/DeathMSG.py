9f8d502b5b5866b4e8711eddd94ff0635c8f137a
DreTaX <dretax14@gmail.com>
2014-12-16 19:54:17 +0100

    3.0

--- DeathMSG/DeathMSG.py
+++ DeathMSG/DeathMSG.py
@@ -230,9 +230,9 @@ class DeathMSG:
 
     def Log(self, killer, weapon, dist, victim, body, dmg, tp):
         if tp is None:
-            Plugin.Log("KillLog", str(System.DateTime.Now) + " Killer: " + killer + " Gun: " + weapon + " Dist: " + str(dist) + " Victim: " + victim + " BodyP: " + body + " DMG: " + str(dmg))
+            Plugin.Log("KillLog", " Killer: " + killer + " Gun: " + weapon + " Dist: " + str(dist) + " Victim: " + victim + " BodyP: " + body + " DMG: " + str(dmg))
         else:
-            Plugin.Log("KillLog", str(System.DateTime.Now) + " Killer: " + killer + " Gun: " + weapon + " Dist: " + str(dist) + " Victim: " + victim + " BodyP: " + body + " DMG: " + str(dmg) + " WAS TELEPORTING")
+            Plugin.Log("KillLog", " Killer: " + killer + " Gun: " + weapon + " Dist: " + str(dist) + " Victim: " + victim + " BodyP: " + body + " DMG: " + str(dmg) + " WAS TELEPORTING")
 
     def IsAnimal(self, killer):
         if killer == 'Wolf' or killer == 'Bear' or killer == 'MutantWolf' or killer == 'MutantBear':
