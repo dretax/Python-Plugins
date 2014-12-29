7c8a1465248cfdc5567b9a821c863a28503d5722
DreTaX <dretax14@gmail.com>
2014-12-29 22:51:56 +0100

    1.0

--- SteamProfiler/SteamProfiler.py
+++ SteamProfiler/SteamProfiler.py
@@ -24,9 +24,15 @@ class SteamProfiler:
             ini.AddSetting("Settings", "AllowShared", "0")
             ini.AddSetting("Settings", "AllowedVACBans", "2")
             ini.AddSetting("Settings", "MaximumDays", "300")
+            ini.AddSetting("Settings", "CheckForOnlyRust", "1")
+            ini.AddSetting("Settings", "CheckForPrivate", "1")
+            ini.AddSetting("Settings", "CheckForNoProfile", "1")
             ini.AddSetting("Settings", "Sys", "SteamProfiler")
             ini.AddSetting("Settings", "UsingShared", "You are using shared RUST. It's not allowed.")
             ini.AddSetting("Settings", "VacBans", "You reached the max number of VAC bans.")
+            ini.AddSetting("Settings", "OnlyRust", "People only having Rust are not allowed to join.")
+            ini.AddSetting("Settings", "NoProfile", "Only People who have setup their community profile can join.")
+            ini.AddSetting("Settings", "Private", "Only People who are having public profile can join.")
             ini.Save()
         return Plugin.GetIni("Ini")
 
@@ -63,6 +69,9 @@ class SteamProfiler:
         AllowVACBanned = int(ini.GetSetting("Settings", "AllowVACBanned"))
         AllowedVACBans = int(ini.GetSetting("Settings", "AllowedVACBans"))
         MaximumDays = int(ini.GetSetting("Settings", "MaximumDays"))
+        CheckOnlyRust = int(ini.GetSetting("Settings", "CheckForOnlyRust"))
+        CheckForPrivate = int(ini.GetSetting("Settings", "CheckForPrivate"))
+        CheckForNoProfile = int(ini.GetSetting("Settings", "CheckForNoProfile"))
         sys = ini.GetSetting("Settings", "Sys")
         if AllowShared == 0:
             url = Web.GET("http://api.steampowered.com/IPlayerService/IsPlayingSharedGame/v0001/?key=" + APIKey + "&steamid=" + id + "&appid_playing=252490")
@@ -73,7 +82,7 @@ class SteamProfiler:
                 Player.Disconnect()
                 return
         if AllowVACBanned == 0:
-            url = Web.GET("http://api.steampowered.com/ISteamUser/GetPlayerBans/v0001/?key=" + APIKey + "&steamids=" + id)
+            url = Web.GET("http://api.steampowered.com/ISteamUser/GetPlayerBans/v0001/?key=" + APIKey + "&steamid=" + id)
             data = self.GetSteamDatas(url, 2)
             numv = data[3].split(':')
             numv = int(numv[1])
@@ -84,3 +93,31 @@ class SteamProfiler:
                 Player.MessageFrom(sys, red + msg)
                 Player.Disconnect()
                 return
+        if CheckOnlyRust == 1:
+            url = Web.GET("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=" + APIKey + "&steamid=" + id)
+            listofappids = re.findall(r'"appid":.*,', url)
+            gamecount = re.findall(r'"game_count":.*,', url)
+            gamecount = re.sub('["\,\ \'\[\]]', '', str(gamecount)).split(':')
+            if int(gamecount[1]) == 1 and "252490" in listofappids:
+                msg = ini.GetSetting("Settings", "OnlyRust")
+                Player.MessageFrom(sys, red + msg)
+                Player.Disconnect()
+                return
+        if CheckForPrivate == 1 or CheckForNoProfile == 1:
+            url = Web.GET("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + APIKey + "&steamid=" + id)
+            if CheckForNoProfile == 1:
+                comm = re.findall(r'"profilestate":.*,', url)
+                comm = re.sub('["\,\ \'\[\]]', '', str(comm)).split(':')
+                if int(comm[1]) != 1:
+                    msg = ini.GetSetting("Settings", "NoProfile")
+                    Player.MessageFrom(sys, red + msg)
+                    Player.Disconnect()
+                    return
+            if CheckForPrivate == 1:
+                comm = re.findall(r'"communityvisibilitystate":.*,', url)
+                comm = re.sub('["\,\ \'\[\]]', '', str(comm)).split(':')
+                if int(comm[1]) == 1:
+                    msg = ini.GetSetting("Settings", "Private")
+                    Player.MessageFrom(sys, red + msg)
+                    Player.Disconnect()
+                    return
