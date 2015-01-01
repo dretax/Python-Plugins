8b554c1339e5a63233714550ebcbf963bd8ac2a3
DreTaX <dretax14@gmail.com>
2015-01-01 23:02:07 +0100

    Todoooooo..........

---
+++ TpFriend/TpFriend.py
@@ -0,0 +1,19 @@
+__author__ = 'DreTaX'
+__version__ = '3.0'
+import clr
+
+clr.AddReferenceByPartialName("Fougerite")
+import Fougerite
+import math
+import System
+from System import *
+
+"""
+    Class
+"""
+
+class TpFriend:
+    """
+        Methods
+    """
+    #Todo:.............
