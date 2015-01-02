444a3a8240da48ffcf366364586eecd1deb81c59
DreTaX <dretax14@gmail.com>
2015-01-02 18:28:07 +0100

    1.2

--- C4Log/C4Log.py
+++ C4Log/C4Log.py
@@ -1,5 +1,5 @@
 __author__ = 'DreTaX'
-__version__ = '1.1'
+__version__ = '1.2'
 import clr
 
 clr.AddReferenceByPartialName("Fougerite")
@@ -33,6 +33,17 @@ class C4Log:
         except:
             return None
 
+    #There is an error while converting ownerid to string in C#. Hax it.
+    def GetIt(self, Entity):
+        try:
+            if Entity.IsDeployableObject():
+                return Entity.Object.ownerID
+            if Entity.IsStructure():
+                return Entity.Object._master.ownerID
+        except:
+            return None
+
+
     def On_EntityHurt(self, HurtEvent):
         if HurtEvent.Attacker is not None and HurtEvent.Entity is not None and not HurtEvent.IsDecay and HurtEvent.DamageType is not None:
             #On Entity hurt the attacker is an NPC and a Player for some reason. We will try to grab his ID
@@ -41,7 +52,9 @@ class C4Log:
                 return
             if HurtEvent.Entity.IsStructure() or HurtEvent.Entity.IsDeployableObject():
                 if HurtEvent.DamageType == "Explosion":
+                    entityloc = str(Util.CreateVector(HurtEvent.Entity.X, HurtEvent.Entity.Y, HurtEvent.Entity.Z))
+                    entityid = str(self.GetIt(HurtEvent.Entity))
                     if HurtEvent.WeaponName == "Explosive Charge":
-                        Plugin.Log("C4", str(HurtEvent.Attacker.Location) + " | " + HurtEvent.Attacker.Name)
+                        Plugin.Log("C4", str(HurtEvent.Attacker.Location) + " | " + HurtEvent.Attacker.Name + " | " + id + " | Entity: " + HurtEvent.Entity.Name + " | " + entityloc + " | " + entityid)
                     else:
-                        Plugin.Log("Grenade", str(HurtEvent.Attacker.Location) + " | " +  HurtEvent.Attacker.Name)
+                        Plugin.Log("Grenade", str(HurtEvent.Attacker.Location) + " | " +  HurtEvent.Attacker.Name + " | " + id + " | Entity: " + HurtEvent.Entity.Name + " | " + entityloc + " | " + entityid)
