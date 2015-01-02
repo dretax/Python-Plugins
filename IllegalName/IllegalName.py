460cd5931be70de5e9f383f8798ea64e9f0faf56
DreTaX <dretax14@gmail.com>
2015-01-02 02:17:39 +0100

    I was drugged when I wrote this.

--- IllegalName/IllegalName.py
+++ IllegalName/IllegalName.py
@@ -1,15 +1,16 @@
 __author__ = 'DreTaX'
-__version__ = '1.0a'
+__version__ = ''
 import clr
 
 clr.AddReferenceByPartialName("Fougerite")
 import Fougerite
 import re
 
+
 class IllegalName:
 
     def On_PluginInit(self):
-        Util.ConsoleLog("IllegalName by " + __author__ + " Version: " + __version__ + " loaded.", False)
+        Util.ConsoleLog("BannedPeople by " + __author__ + " Version: " + __version__ + " loaded.", False)
 
     def getIllegal(self):
         if not Plugin.IniExists("IllegalNames"):
@@ -20,13 +21,6 @@ class IllegalName:
             IllegalNames.Save()
         return Plugin.GetIni("IllegalNames")
 
-    def TrytoGrabName(self, Player):
-        try:
-            name = Player.Name
-            return name
-        except:
-            Plugin.Log("IllegalNErr", "Error caught at TrytoGrabName method.")
-            return None
 
     def IllegalNameConfig(self):
         if not Plugin.IniExists("IllegalNameConfig"):
@@ -34,42 +28,36 @@ class IllegalName:
             loc.Save()
         return Plugin.GetIni("IllegalNameConfig")
 
+
     def On_PlayerConnected(self, Player):
-        name = self.TrytoGrabName(Player)
-        if name == None:
-            return
+        name = Player.Name
         n = len(name)
-        ini = self.IllegalNameConfig()
-        illini = self.getIllegal()
-        f = int(ini.GetSetting("options", "protection1"))
-        reason = ini.GetSetting("options", "DisconnectReason")
-        reason2 = ini.GetSetting("options", "DisconnectReason2")
-        space = ini.GetSetting("options", "Spaces")
-        listnames = illini.EnumSection("IllegalNames")
-        counted = len(listnames)
-        i = 0
-        if counted > 0:
-            for checkn in listnames:
-                get = illini.GetSetting("IllegalNames", checkn)
-                i += 1
-                lowername = Data.ToLower(name)
-                lowercheck = Data.ToLower(get)
-                if counted >= i:
-                    if lowercheck in lowername:
-                        Player.Message(reason)
-                        Player.Disconnect()
-                        return
+        ini = self.getIllegal()
+        config = self.IllegalNameConfig()
+        f = int(config.GetSetting("Settings", "Protection"))
+        reason = config.GetSetting("Settings", "DisconnectReason")
+        reason2 = config.GetSetting("Settings", "DisconnectReason2")
+        space = int(config.GetSetting("Settings", "Spaces"))
+        listnames = ini.EnumSection("IllegalNames")
+        for checkn in listnames:
+            get = ini.GetSetting("IllegalNames", checkn)
+            lowername = name.lower()
+            lowercheck = get.lower()
+            if lowercheck in lowername:
+                Player.Message(reason)
+                Player.Disconnect()
+                return
         if f == 1:
             if space == 0:
-                a = re.match('^[a-zA-Z0-9_!+?%éáűőúöüó()<>/\@#,.\[\]-]+$', name)
-                if not a:
+                a = re.compile("^(a-zA-Z0-9!@#\$%\^\&*\[\]\)\<\>\(+=._-)*$")
+                if not a.match(name):
                     Player.Message(reason2)
                     Player.Message("Allowed Chars: a-z,0-9,!@#$%/\[]<>+=.-")
                     Player.Message("Spaces are not allowed")
                     Player.Disconnect()
             else:
-                a = re.match('^[a-zA-Z0-9_ !+?%éáűőúöüó()<>/\@#,.\[\]-]+$', name)
-                if not a or n <= 1:
+                a = re.compile("^(a-zA-Z0-9!@#\$%\^\&*\[\]\)\ \<\>\(+=._-)*$")
+                if not a.match(name) or n <= 1:
                     Player.Message(reason2)
                     Player.Message("Allowed Chars: a-z,0-9,!@#$%/\[]<>+=.-")
                     Player.Disconnect()
@@ -79,16 +67,16 @@ class IllegalName:
                 Player.Message("Allowed Chars: a-z,0-9,!@#$%/\[]<>+=.-")
                 Player.Disconnect()
         elif f == 3:
-            if (space == 0):
-                a = re.match('^[a-zA-Z0-9_!+?%éáűőúöüó()<>/\@#,.\[\]-]+$', name)
-                if not a or n <= 1:
+            if space == 0:
+                a = re.compile("^(a-zA-Z0-9!@#\$%\^\&*\[\]\)\<\>\(+=._-)*$")
+                if not a.match(name) or n <= 1:
                     Player.Message(reason2)
                     Player.Message("Allowed Chars: a-z,0-9,!@#$%/\[]<>+=.-")
                     Player.Message("Spaces are not allowed")
                     Player.Disconnect()
             else:
-                a = re.match('^[a-zA-Z0-9_ !+?%éáűőúöüó()<>/\@#,.\[\]-]+$', name)
-                if not a or n <= 1:
+                a = re.compile("^(a-zA-Z0-9!@#\$%\^\&*\[\]\)\ \<\>\(+=._-)*$")
+                if not a.match(name) or n <= 1:
                     Player.Message(reason2)
                     Player.Message("Allowed Chars: a-z,0-9,!@#$%/\[]<>+=.-")
                     Player.Disconnect()
