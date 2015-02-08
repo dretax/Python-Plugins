697edd5309dd04722b5adc3569669b2538a34f3b
DreTaX <dretax14@gmail.com>
2015-02-08 21:01:50 +0100

    1.4

--- SleeperLog/SleeperLog.py
+++ SleeperLog/SleeperLog.py
@@ -1,5 +1,5 @@
 __author__ = 'DreTaX'
-__version__ = '1.3'
+__version__ = '1.4'
 
 import clr
 
@@ -57,22 +57,18 @@ class SleeperLog:
             ini.SetSetting("Sleeper", id, name)
             ini.Save()
 
-
-    def On_EntityHurt(self, HurtEvent):
-        if HurtEvent.Attacker is not None and HurtEvent.Entity is not None and not HurtEvent.IsDecay:
-            #On Entity hurt the attacker is an NPC and a Player for some reason. We will try to grab his ID
+    def On_PlayerHurt(self, HurtEvent):
+        if HurtEvent.Attacker is not None and HurtEvent.Victim is not None:
             id = self.TrytoGrabID(HurtEvent.Attacker)
             if id is None:
                 return
-            entityname = HurtEvent.Entity.Name
-            if entityname == "MaleSleeper":
-                #Dirty fucking hack against current bug. (Entity OWNERID request isn't working good yet, so hax it)
-                OwnerID = self.GetIt(HurtEvent.Entity)
-                if OwnerID is None:
-                    return
-                entityloc = Util.CreateVector(HurtEvent.Entity.X, HurtEvent.Entity.Y, HurtEvent.Entity.Z)
-                ini = self.SleeperId()
-                name = ini.GetSetting("Sleeper", str(OwnerID))
-                if name is not None:
-                    attacker = HurtEvent.Attacker.Name
-                    Plugin.Log("SleeperLog", str(entityloc) + "|" + attacker + "|<- Attacker|" + id + "|" + name)
+            #entity = str(HurtEvent.Entity.Name)
+            OwnerID = self.GetIt(HurtEvent.Entity)
+            if OwnerID is None:
+                return
+            entityloc = Util.CreateVector(HurtEvent.Entity.X, HurtEvent.Entity.Y, HurtEvent.Entity.Z)
+            ini = self.SleeperId()
+            name = ini.GetSetting("Sleeper", str(OwnerID))
+            if name is not None:
+                attacker = HurtEvent.Attacker.Name
+                Plugin.Log("SleeperLog", str(entityloc) +  + "|" + name + "|" + attacker + "|<- Attacker|" + id + "|")
