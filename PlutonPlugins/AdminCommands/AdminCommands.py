0a8b97b1d8bd288fa5c3688a16a6af8449b28415
DreTaX <dretax14@gmail.com>
2015-03-14 14:55:11 +0100

    1.6

--- PlutonPlugins/AdminCommands/AdminCommands.py
+++ PlutonPlugins/AdminCommands/AdminCommands.py
@@ -8,6 +8,10 @@ import Pluton
 import System
 from System import *
 import math
+import sys
+path = Util.GetPublicFolder()
+sys.path.append(path + "\\Python\\Lib\\")
+import hashlib
 
 """
     Class
@@ -16,9 +20,22 @@ import math
 
 class AdminCommands:
 
+    hash = None
+
+    def On_PluginInit(self):
+        ini = self.AdminCmdConfig()
+        password = ini.GetSetting("Settings", "OwnerPassword")
+        if password == "SetThisToSomethingElse":
+            return
+        hashed = hashlib.md5(password).hexdigest()
+        ini.SetSetting("Settings", "OwnerPassword", hashed)
+        ini.Save()
+        self.hash = hashed
+
     def AdminCmdConfig(self):
         if not Plugin.IniExists("AdminCmdConfig"):
             loc = Plugin.CreateIni("AdminCmdConfig")
+            loc.AddSetting("Settings", "OwnerPassword", "SetThisToSomethingElse")
             loc.Save()
         return Plugin.GetIni("AdminCmdConfig")
 
@@ -28,7 +45,6 @@ class AdminCommands:
             if pl.Name.lower() == name:
                 return pl
         return None
-
 
     """
         CheckV method based on Spock's method.
@@ -126,7 +142,7 @@ class AdminCommands:
             if len(args) <= 1:
                 Player.Message("Usage: /mute playername minutes")
                 return
-            pl = self.CheckV(Player, args)
+            pl = self.CheckV(Player, args[0])
             if pl is not None:
                 if not args[1].isdigit():
                     Player.Message("Usage: /mute playername minutes")
@@ -180,22 +196,122 @@ class AdminCommands:
                 return
             text = str.join(' ', args)
             Server.BroadcastFrom("Server", text)
+        elif cmd.cmd == "clear":
+            if not Player.Admin:
+                Player.Message("You aren't an admin!")
+                return
+            for x in Player.Inventory.AllItems():
+                x._item.Remove(1)
+            Player.Message("Cleared!")
+        elif cmd.cmd == "repairall":
+            if not Player.Admin:
+                Player.Message("You aren't an admin!")
+                return
+            for x in Player.Inventory.AllItems():
+                x._item.RepairCondition(x._item.maxCondition)
+            Player.Message("Repaired All!")
+        elif cmd.cmd == "give":
+            if not Player.Admin:
+                Player.Message("You aren't an admin!")
+                return
+            if len(args) <= 1:
+                Player.Message("Usage: /give playername item amount")
+                return
+            pl = self.CheckV(Player, args[0])
+            if pl is not None:
+                if len(args) == 2:
+                    num = 1
+                else:
+                    if not args[2].isdigit():
+                        num = 1
+                    else:
+                        num = int(args[2])
+                item = args[1]
+                pl.Inventory.Add(item, num)
+        elif cmd.cmd == "i":
+            if not Player.Admin:
+                Player.Message("You aren't an admin!")
+                return
+            if len(args) == 0:
+                Player.Message("Usage: /i item amount")
+                return
+            if len(args) == 1:
+                num = 1
+            else:
+                if not args[1].isdigit():
+                    num = 1
+                else:
+                    num = int(args[1])
+            item = args[0]
+            Player.Inventory.Add(item, num)
+        elif cmd.cmd == "addowner":
+            if not Player.Owner:
+                Player.Message("You aren't an owner!")
+                return
+            if len(args) == 0:
+                Player.Message("Usage: /addowner name")
+                return
+            pl = self.CheckV(Player, args)
+            if pl is not None:
+                pl.MakeOwner(Player.Name + " made " + pl.Name + " an owner")
+                Player.Message(pl.Name + " is now an owner!")
+                pl.Message(Player.Name + " made you an owner!")
+        elif cmd.cmd == "addmoderator":
+            if not Player.Owner:
+                Player.Message("You aren't an owner!")
+                return
+            if len(args) == 0:
+                Player.Message("Usage: /addmoderator name")
+                return
+            pl = self.CheckV(Player, args)
+            if pl is not None:
+                pl.MakeModerator(Player.Name + " made " + pl.Name + " a moderator")
+                Player.Message(pl.Name + " is now a moderator!")
+                pl.Message(Player.Name + " made you a moderator!")
+        elif cmd.cmd == "removerights":
+            if not Player.Owner:
+                Player.Message("You aren't an owner!")
+                return
+            if len(args) == 0:
+                Player.Message("Usage: /removerights name")
+                return
+            pl = self.CheckV(Player, args)
+            if pl is not None:
+                pl.MakeNone()
+                Player.Message("You removed " + pl.Name + "'s rights.")
+                pl.Message(Player.Name + " removed your rights.")
+        elif cmd.cmd == "getowner":
+            if self.hash is None:
+                return
+            if len(args) == 0:
+                Player.Message("Usage: /getowner password")
+                return
+            text = str.join(' ', args)
+            hash = hashlib.md5(text).hexdigest()
+            if hash == self.hash:
+                Player.Message("You gained owner.")
+                Player.MakeOwner(Player.Name + " made himself owner via /getowner")
+                return
+            Player.Message("That didn't work buddy.")
+
 
     def On_DoorUse(self, DoorEvent):
         if DataStore.Get("adoor", DoorEvent.Player.SteamID):
             DoorEvent.Open = True
 
-    def On_Chat(self, Player, args):
-        if DataStore.ContainsKey("MuteList", Player.SteamID):
-            time = int(DataStore.Get("MuteListT", Player.SteamID))
+    def On_Chat(self, args):
+        if DataStore.ContainsKey("MuteList", args.User.SteamID):
+            Player = args.User
+            id = Player.SteamID
+            time = int(DataStore.Get("MuteListT", id))
             calc = System.Environment.TickCount - (time * 60000)
             if calc < 0 or math.isnan(calc) or math.isnan(time):
-                DataStore.Remove("MuteListT", Player.SteamID)
-                DataStore.Remove("MuteList", Player.SteamID)
+                DataStore.Remove("MuteListT", id)
+                DataStore.Remove("MuteList", id)
                 return
             if calc >= time * 60000:
-                DataStore.Remove("MuteListT", Player.SteamID)
-                DataStore.Remove("MuteList", Player.SteamID)
+                DataStore.Remove("MuteListT", id)
+                DataStore.Remove("MuteList", id)
                 return
             done = round((calc / 1000) / 60, 2)
             Player.Message("Mute Cooldown: " + str(done) + "/" + str(time))
@@ -207,4 +323,4 @@ class AdminCommands:
             return
         attacker = EntityHurtEvent.Attacker.ToPlayer()
         if DataStore.ContainsKey("Instako", attacker.SteamID):
-            EntityHurtEvent.Victim.ToBuildingPart().Health = 0
+            EntityHurtEvent.Victim.ToBuildingPart().Destroy()
