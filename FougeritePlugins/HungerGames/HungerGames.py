1d78a8b6be68046f77733e6a7348b9d69824e204
DreTaX <dretax14@gmail.com>
2015-05-29 21:49:35 +0200

    1.4

--- FougeritePlugins/HungerGames/HungerGames.py
+++ FougeritePlugins/HungerGames/HungerGames.py
@@ -472,7 +472,7 @@ class HungerGames:
                             Server.BroadcastFrom(sysname, purple + "Detected " + str(minp) + " players.")
                             Server.BroadcastFrom(sysname, purple + "Forcing game start in " + str(MinimumTime) +
                                                  " minutes.")
-                            Plugin.CreateTimer("Force", MinimumTime * 1000).Start()
+                            Plugin.CreateTimer("Force", MinimumTime * 60000).Start()
                         self.StartGame()
                 elif arg == "leave":
                     if not self.IsActive:
