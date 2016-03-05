3aed85bfbf23e87b877006f3f678a0c5283f27e3
DreTaX <dretax@gmail.hu>
2016-03-05 13:57:09 +0100

    1.1

--- FougeritePlugins/AntiAdvert/AntiAdvert.py
+++ FougeritePlugins/AntiAdvert/AntiAdvert.py
@@ -1,5 +1,5 @@
 __author__ = 'DreTaX'
-__version__ = '1.0'
+__version__ = '1.1'
 import clr
 
 clr.AddReferenceByPartialName("Fougerite")
@@ -15,10 +15,13 @@ class AntiAdvert:
         if Player.Admin or Player.Moderator:
             return
         Text = ChatEvent.OriginalMessage.lower()
-        Text = re.sub(r'[0-9]+(?:\.[0-9]+){3}', '', Text)
-        a = re.findall(r'([\.a-z]*\.)(com|en|org|de|ro|ru|hu|eu)', Text)
+        Text = re.sub(r'[0-9]+(?:\.[0-9]+){3}(:[0-9]+)?', '', Text)
+        a = re.findall(r'([\.a-z]*\.)(com|en|org|de|ro|ru|hu|eu|net)(:[0-9]+)', Text)
         for x in a:
             n = str.join('', x)
             if n not in whitelist:
                 Text = Text.replace(n, '')
+        if ChatEvent.OriginalMessage.lower() != Text:
+            Plugin.Log("Test", "- " + Player.Name + " : " + ChatEvent.OriginalMessage.lower())
+            Player.Message("Please do not advertise!")
         ChatEvent.NewText = Text
