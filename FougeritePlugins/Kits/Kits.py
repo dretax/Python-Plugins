05d96b7f2bf6a3468f50611bd211290bae37e7e7
DreTaX <dretax@gmail.hu>
2018-03-29 10:53:42 +0200

    1.2

--- FougeritePlugins/Kits/Kits.py
+++ FougeritePlugins/Kits/Kits.py
@@ -1,5 +1,5 @@
 __author__ = 'DreTaX'
-__version__ = '1.1'
+__version__ = '1.2'
 
 import clr
 clr.AddReferenceByPartialName("Fougerite")
@@ -19,15 +19,28 @@ path = Util.GetRootFolder()
 
 class GivenKit:
 
+    Enabled = True
+    Cooldown = 0
     AdminCanUse = False
     ModeratorCanUse = False
     NormalCanUse = False
+    AutoGiveOnSpawn = False
+    AdminCanBypassCooldown = False
+    ModeratorCanBypassCooldown = False
+    ClearInvOnUse = False
     ItemsDict = None
 
-    def __init__(self, AdminCanUse, ModeratorCanUse, NormalCanUse, ItemsDict):
+    def __init__(self, Enabled, Cooldown, AdminCanUse, ModeratorCanUse, NormalCanUse, ItemsDict, AutoGiveOnSpawn,
+                 AdminCanBypassCooldown, ModeratorCanBypassCooldown, ClearInvOnUse):
+        self.Enabled = Enabled
+        self.Cooldown = Cooldown
         self.AdminCanUse = AdminCanUse
         self.ModeratorCanUse = ModeratorCanUse
         self.NormalCanUse = NormalCanUse
+        self.AutoGiveOnSpawn = AutoGiveOnSpawn
+        self.AdminCanBypassCooldown = AdminCanBypassCooldown
+        self.ModeratorCanBypassCooldown = ModeratorCanBypassCooldown
+        self.ClearInvOnUse = ClearInvOnUse
         self.ItemsDict = ItemsDict
 
 KitStore = {
@@ -37,17 +50,11 @@ KitStore = {
 
 class Kits:
 
-    def KitsConfig(self):
-        if not Plugin.IniExists("KitsConfig"):
-            loc = Plugin.CreateIni("KitsConfig")
-            loc.AddSetting("AdminKits", "AvailableKits", "starter, admin")
-            loc.AddSetting("PlayerKits", "AvailableKits", "starter:120000")
-            # loc.AddSetting("PlayerKits", "DefaultKits", "starter:True,AdminKit:False")
-            loc.Save()
-        return Plugin.GetIni("KitsConfig")
-
     def On_PluginInit(self):
-        self.KitsConfig()
+        Files = System.IO.Directory.GetFiles(path + "\\Save\\PyPlugins\\Kits\\\LoadOuts\\", "*.ini",
+                                             System.IO.SearchOption.AllDirectories)
+        for x in Files:
+            self.GetKitDataWithPath(x)
 
     def GetStringFromArray(self, Array, String):
         matching = str([s for s in Array if String in s])
@@ -68,84 +75,151 @@ class Kits:
             return KitStore[name]
         kit = Plugin.GetIni(path + "\\Save\\PyPlugins\\Kits\\\LoadOuts\\" + name)
         if kit is not None:
+            Enabled = self.bool(kit.GetSetting("Kit", "Enabled"))
+            Cooldown = int(kit.GetSetting("Kit", "Cooldown"))
             Admin = self.bool(kit.GetSetting("Kit", "AdminCanUse"))
             Moderator = self.bool(kit.GetSetting("Kit", "ModeratorCanUse"))
             Normal = self.bool(kit.GetSetting("Kit", "NormalCanUse"))
+            AutoGiveOnSpawn = self.bool(kit.GetSetting("Kit", "AutoGiveOnSpawn"))
+            AdminCanBypassCooldown = self.bool(kit.GetSetting("Kit", "AdminCanBypassCooldown"))
+            ModeratorCanBypassCooldown = self.bool(kit.GetSetting("Kit", "ModeratorCanBypassCooldown"))
+            ClearInvOnUse = self.bool(kit.GetSetting("Kit", "ClearInvOnUse"))
             Items = kit.EnumSection("Items")
             dictt = {}
             for x in Items:
                 l = int(kit.GetSetting("Items", x))
                 dictt[x] = l
-            c = GivenKit(Admin, Moderator, Normal, dictt)
+            c = GivenKit(Enabled, Cooldown, Admin, Moderator, Normal, dictt, AutoGiveOnSpawn, AdminCanBypassCooldown,
+                         ModeratorCanBypassCooldown, ClearInvOnUse)
             KitStore[name] = c
             return c
         return None
 
+    def GetKitDataWithPath(self, path):
+        name = System.IO.GetFileName(path)
+        if name in KitStore.keys():
+            return KitStore[name]
+        kit = Plugin.GetIni(path)
+        if kit is not None:
+            Enabled = self.bool(kit.GetSetting("Kit", "Enabled"))
+            Cooldown = int(kit.GetSetting("Kit", "Cooldown"))
+            Admin = self.bool(kit.GetSetting("Kit", "AdminCanUse"))
+            Moderator = self.bool(kit.GetSetting("Kit", "ModeratorCanUse"))
+            Normal = self.bool(kit.GetSetting("Kit", "NormalCanUse"))
+            AutoGiveOnSpawn = self.bool(kit.GetSetting("Kit", "AutoGiveOnSpawn"))
+            AdminCanBypassCooldown = self.bool(kit.GetSetting("Kit", "AdminCanBypassCooldown"))
+            ModeratorCanBypassCooldown = self.bool(kit.GetSetting("Kit", "ModeratorCanBypassCooldown"))
+            ClearInvOnUse = self.bool(kit.GetSetting("Kit", "ClearInvOnUse"))
+            Items = kit.EnumSection("Items")
+            dictt = {}
+            for x in Items:
+                l = int(kit.GetSetting("Items", x))
+                dictt[x] = l
+            c = GivenKit(Enabled, Cooldown, Admin, Moderator, Normal, dictt, AutoGiveOnSpawn, AdminCanBypassCooldown,
+                         ModeratorCanBypassCooldown, ClearInvOnUse)
+            KitStore[name] = c
+            return c
+        return
+
+    def GiveKit(self, data, inventory):
+        for x in data.ItemsDict.keys():
+            inventory.AddItem(x, data.ItemsDict[x])
+
     def On_Command(self, Player, cmd, args):
         if cmd == "kit" or cmd == "kits":
-            ini = self.KitsConfig()
+            if len(args) == 0 or len(args) > 1:
+                kitnames = ""
+                for x in KitStore.keys():
+                    kitnames = kitnames + x + ", "
+                Player.MessageFrom("Kits", "Available Kits: " + kitnames)
+                Player.MessageFrom("Kits", "Commands: /kit kitname")
+                return
+            inv = Player.Inventory
             if Player.Admin or Player.Moderator:
-                akits = ini.GetSetting("AdminKits", "AvailableKits")
-                if len(args) == 0:
-                    Player.MessageFrom("Kits", "Available Kits: " + akits)
-                    Player.MessageFrom("Kits", "Commands: /kit kitname")
-                    return
                 data = self.GetKitData(args[0])
                 if data is not None:
+                    if not data.Enabled:
+                        Player.MessageFrom("Kits", "This kit has been disabled.")
+                        return
                     if (Player.Admin and data.AdminCanUse) or (Player.Moderator and data.ModeratorCanUse):
-                        inv = Player.Inventory
-                        for x in data.ItemsDict.keys():
-                            inv.AddItem(x, data.ItemsDict[x])
+                        # If our Player is an admin and cannot bypass the Kit's Cooldown.
+                        if Player.Admin and data.AdminCanUse and data.Cooldown > 0 and not data.AdminCanBypassCooldown:
+                            cooldown = data.Cooldown / 60000 * 60
+                            systick = TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds
+                            time = DataStore.Get("KitCooldown" + str(args[0]), Player.UID)
+                            if time is None:
+                                DataStore.Add("KitCooldown" + str(args[0]), Player.UID, 7)
+                                time = 7
+                            calc = systick - time
+                            if calc < cooldown and time != 7:
+                                Player.MessageFrom("Kits", "You have to wait before using this again!")
+                                done = round(calc)
+                                done2 = round(cooldown, 2)
+                                Player.MessageFrom("Kits", "Time Remaining: " + str(done) + " / " + str(done2) +
+                                                   " seconds")
+                                return
+                            DataStore.Add("KitCooldown" + str(args[0]), Player.UID,
+                                          TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds)
+                        # If our Player is a Moderator and cannot bypass the Kit's Cooldown.
+                        if Player.Moderator and not Player.Admin and data.ModeratorCanUse and data.Cooldown > 0 \
+                                and not data.ModeratorCanBypassCooldown:
+                            cooldown = data.Cooldown / 60000 * 60
+                            systick = TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds
+                            time = DataStore.Get("KitCooldown" + str(args[0]), Player.UID)
+                            if time is None:
+                                DataStore.Add("KitCooldown" + str(args[0]), Player.UID, 7)
+                                time = 7
+                            calc = systick - time
+                            if calc < cooldown and time != 7:
+                                Player.MessageFrom("Kits", "You have to wait before using this again!")
+                                done = round(calc)
+                                done2 = round(cooldown, 2)
+                                Player.MessageFrom("Kits", "Time Remaining: " + str(done) + " / " + str(done2) +
+                                                   " seconds")
+                                return
+                            DataStore.Add("KitCooldown" + str(args[0]), Player.UID,
+                                          TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds)
+                        if data.ClearInvOnUse:
+                            inv.Clear()
+                        self.GiveKit(data, inv)
                         Player.MessageFrom("Kits", "Kit " + args[0] + " received!")
                     else:
                         Player.MessageFrom("Kits", "You can't use this!")
                 else:
                     Player.MessageFrom("Kits", "Kit " + args[0] + " not found!")
             else:
-                pkits = ini.GetSetting("PlayerKits", "AvailableKits")
-                array = pkits.split(',')
-                if len(args) == 0:
-                    leng = len(array)
-                    i = 0
-                    String = ''
-                    for x in array:
-                        if i <= leng:
-                            x = x.split(':')
-                            String = String + x[0] + ', '
-                    Player.MessageFrom("Kits", "Available Kits: " + String)
-                    return
                 data = self.GetKitData(args[0])
                 if data is None:
                     Player.MessageFrom("Kits", "Kit " + str(args[0]) + " not found!")
                     return
+                if not data.Enabled:
+                    Player.MessageFrom("Kits", "This kit has been disabled.")
+                    return
                 if not data.NormalCanUse:
                     Player.MessageFrom("Kits", "You can't get this!")
                     return
-                get = self.GetStringFromArray(array, str(args[0]))
-                get = re.sub('[[\]\']+', '', get).split(':')
-                cooldown = int(get[1]) / 60000 * 60
-                if cooldown > 0:
+                if data.Cooldown > 0:
+                    cooldown = data.Cooldown / 60000 * 60
                     systick = TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds
-                    time = DataStore.Get("startercooldown" + str(args[0]), Player.UID)
+                    time = DataStore.Get("KitCooldown" + str(args[0]), Player.UID)
                     if time is None:
-                        DataStore.Add("startercooldown" + str(args[0]), Player.UID, 7)
+                        DataStore.Add("KitCooldown" + str(args[0]), Player.UID, 7)
                         time = 7
                     calc = systick - time
                     if calc >= cooldown or time == 7:
-                        inv = Player.Inventory
-                        for x in data.ItemsDict.keys():
-                            inv.AddItem(x, data.ItemsDict[x])
+                        if data.ClearInvOnUse:
+                            inv.Clear()
+                        self.GiveKit(data, inv)
                         Player.MessageFrom("Kits", "Kit " + args[0] + " received!")
-                        DataStore.Add("startercooldown" + str(args[0]), Player.UID, TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds)
+                        DataStore.Add("KitCooldown" + str(args[0]), Player.UID,
+                                      TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds)
                     else:
                         Player.MessageFrom("Kits", "You have to wait before using this again!")
                         done = round(calc)
                         done2 = round(cooldown, 2)
                         Player.MessageFrom("Kits", "Time Remaining: " + str(done) + " / " + str(done2) + " seconds")
                 else:
-                    inv = Player.Inventory
-                    for x in data.ItemsDict.keys():
-                        inv.AddItem(x, data.ItemsDict[x])
+                    self.GiveKit(data, inv)
                     Player.MessageFrom("Kits", "Kit " + args[0] + " received!")
         elif cmd == "flushkit":
             if Player.Admin:
@@ -153,5 +227,5 @@ class Kits:
                     Player.MessageFrom("Kits", "/flushkit kitname")
                     return
                 text = str.join("", args)
-                DataStore.Flush("startercooldown" + text)
-                Player.MessageFrom("Kits", "Flushed!")
+                DataStore.Flush("KitCooldown" + text)
+                Player.MessageFrom("Kits", "Flushed all cooldowns for " + text + "!")
