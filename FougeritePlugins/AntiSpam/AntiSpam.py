e4b7d55d131024d44893c97de04ba77b76fd96f8
DreTaX <dretax@gmail.hu>
2018-04-21 23:08:01 +0200

    AntiSpam

---
+++ FougeritePlugins/AntiSpam/AntiSpam.py
@@ -0,0 +1,29 @@
+__author__ = 'DreTaX'
+__version__ = '1.0'
+
+import clr
+clr.AddReferenceByPartialName("Fougerite")
+
+import Fougerite
+import System
+from System import *
+
+Cooldown = {}
+# Seconds that must pass after a player's message before he can send another one.
+FloodSeconds = 2
+
+class AntiSpam:
+
+    def On_Chat(self, Player, ChatEvent):
+        if Player.UID not in Cooldown:
+            Cooldown[Player.UID] = TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds
+        else:
+            calc = TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds - Cooldown[Player.UID]
+            if calc < FloodSeconds:
+                Player.Message("WAIT A BIT BEFORE SENDING ANOTHER MESSAGE...")
+            else:
+                Cooldown[Player.UID] = TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds
+
+    def On_PlayerDisconnected(self, Player):
+        if Player.UID in Cooldown.keys():
+            Cooldown.pop(Player.UID)
