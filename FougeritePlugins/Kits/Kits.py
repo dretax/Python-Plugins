8babc851c8a3e8a9ba34be49a362c9afaf3e4460
DreTaX <dretax@gmail.hu>
2018-05-21 15:55:36 +0200

    Fix

--- FougeritePlugins/Kits/Kits.py
+++ FougeritePlugins/Kits/Kits.py
@@ -1,5 +1,5 @@
 __author__ = 'DreTaX'
-__version__ = '1.3.1'
+__version__ = '1.3.2'
 
 import clr
 clr.AddReferenceByPartialName("Fougerite")
@@ -52,6 +52,7 @@ class GivenKit:
         self.MaxUses = MaxUses
         self.ItemsDict = ItemsDict
 
+
 KitStore = {
 
 }
@@ -60,7 +61,7 @@ KitStore = {
 class Kits:
 
     def On_PluginInit(self):
-        Files = System.IO.Directory.GetFiles(path + "\\Save\\PyPlugins\\Kits\\\LoadOuts\\", "*.ini",
+        Files = System.IO.Directory.GetFiles(path + "\\Save\\PyPlugins\\Kits\\LoadOuts\\", "*.ini",
                                              System.IO.SearchOption.AllDirectories)
         for x in Files:
             self.GetKitDataWithPath(x)
@@ -80,6 +81,7 @@ class Kits:
             raise ValueError("[Kits] Config value is not a boolean!")
 
     def GetKitData(self, name):
+        name = name.lower()
         if name in KitStore.keys():
             return KitStore[name]
         kit = Plugin.GetIni(path + "\\Save\\PyPlugins\\Kits\\\LoadOuts\\" + name)
@@ -108,11 +110,12 @@ class Kits:
             return c
         return None
 
-    def GetKitDataWithPath(self, path):
-        name = System.IO.Path.GetFileNameWithoutExtension(path)
-        if name in KitStore.keys():
-            return KitStore[name]
-        kit = Plugin.GetIni(path)
+    def GetKitDataWithPath(self, ppath):
+        name = System.IO.Path.GetFileNameWithoutExtension(ppath)
+        tname = name.lower()
+        if tname in KitStore.keys():
+            return KitStore[tname]
+        kit = Plugin.GetIni(path + "\\Save\\PyPlugins\\Kits\\\LoadOuts\\" + name)
         if kit is not None:
             Enabled = self.bool(kit.GetSetting("Kit", "Enabled"))
             Cooldown = int(kit.GetSetting("Kit", "Cooldown"))
@@ -134,9 +137,9 @@ class Kits:
                 dictt[x] = l
             c = GivenKit(Enabled, Cooldown, Admin, Moderator, Normal, dictt, AutoGiveOnSpawn, AdminCanBypassCooldown,
                          ModeratorCanBypassCooldown, ClearInvOnUse, MaxUses, AdminCanBypassMaxUses, ModeratorCanBypassMaxUses, OnlyGiveOnSpawnIfHavingDefaultItems)
-            KitStore[name] = c
+            KitStore[name.lower()] = c
             return c
-        return
+        return None
 
     def GiveKit(self, data, inventory):
         for x in data.ItemsDict.keys():
@@ -177,21 +180,21 @@ class Kits:
     def On_Command(self, Player, cmd, args):
         if cmd == "kit" or cmd == "kits":
             if len(args) == 0 or len(args) > 1:
-                kitnames = ""
-                for x in KitStore.keys():
-                    kitnames = kitnames + x + ", "
+                kitnames = str.join(", ", KitStore.keys())
+                kitnames = kitnames.lower()
                 Player.MessageFrom("Kits", "Available Kits: " + kitnames)
                 Player.MessageFrom("Kits", "Commands: /kit kitname")
                 Player.MessageFrom("Kits", "/kituses kitname")
                 return
             inv = Player.Inventory
+            kitname = args[0].lower()
             if Player.Admin or Player.Moderator:
-                data = self.GetKitData(args[0])
+                data = self.GetKitData(kitname)
                 if data is not None:
                     if not data.Enabled:
                         Player.MessageFrom("Kits", "This kit has been disabled.")
                         return
-                    CurrentUses = DataStore.Get("KitMaxUses" + args[0], Player.UID) or 0
+                    CurrentUses = DataStore.Get("KitMaxUses" + kitname, Player.UID) or 0
                     if CurrentUses:
                         CurrentUses = int(CurrentUses)
                     if data.MaxUses > 0:
@@ -202,9 +205,9 @@ class Kits:
                         if Player.Admin and data.AdminCanUse and data.Cooldown > 0 and not data.AdminCanBypassCooldown:
                             cooldown = data.Cooldown / 60000 * 60
                             systick = TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds
-                            time = DataStore.Get("KitCooldown" + str(args[0]), Player.UID)
+                            time = DataStore.Get("KitCooldown" + kitname, Player.UID)
                             if time is None:
-                                DataStore.Add("KitCooldown" + str(args[0]), Player.UID, 7)
+                                DataStore.Add("KitCooldown" + kitname, Player.UID, 7)
                                 time = 7
                             calc = systick - time
                             if calc < cooldown and time != 7:
@@ -214,16 +217,16 @@ class Kits:
                                 Player.MessageFrom("Kits", "Time Remaining: " + str(done) + " / " + str(done2) +
                                                    " seconds")
                                 return
-                            DataStore.Add("KitCooldown" + str(args[0]), Player.UID,
+                            DataStore.Add("KitCooldown" + kitname, Player.UID,
                                           TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds)
                         # If our Player is a Moderator and cannot bypass the Kit's Cooldown.
                         if Player.Moderator and not Player.Admin and data.ModeratorCanUse and data.Cooldown > 0 \
                                 and not data.ModeratorCanBypassCooldown:
                             cooldown = data.Cooldown / 60000 * 60
                             systick = TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds
-                            time = DataStore.Get("KitCooldown" + str(args[0]), Player.UID)
+                            time = DataStore.Get("KitCooldown" + kitname, Player.UID)
                             if time is None:
-                                DataStore.Add("KitCooldown" + str(args[0]), Player.UID, 7)
+                                DataStore.Add("KitCooldown" + kitname, Player.UID, 7)
                                 time = 7
                             calc = systick - time
                             if calc < cooldown and time != 7:
@@ -233,7 +236,7 @@ class Kits:
                                 Player.MessageFrom("Kits", "Time Remaining: " + str(done) + " / " + str(done2) +
                                                    " seconds")
                                 return
-                            DataStore.Add("KitCooldown" + str(args[0]), Player.UID,
+                            DataStore.Add("KitCooldown" + kitname, Player.UID,
                                           TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds)
                         if data.ClearInvOnUse:
                             inv.Clear()
@@ -241,17 +244,17 @@ class Kits:
                             if CurrentUses >= data.MaxUses:
                                 Player.MessageFrom("Kits", "You have reached the maximum uses of this kit!")
                                 return
-                            DataStore.Add("KitMaxUses" + args[0], Player.UID, CurrentUses + 1)
+                            DataStore.Add("KitMaxUses" + kitname, Player.UID, CurrentUses + 1)
                         self.GiveKit(data, inv)
-                        Player.MessageFrom("Kits", "Kit " + args[0] + " received!")
+                        Player.MessageFrom("Kits", "Kit " + kitname + " received!")
                     else:
                         Player.MessageFrom("Kits", "You can't use this!")
                 else:
-                    Player.MessageFrom("Kits", "Kit " + args[0] + " not found!")
+                    Player.MessageFrom("Kits", "Kit " + kitname + " not found!")
             else:
-                data = self.GetKitData(args[0])
+                data = self.GetKitData(kitname)
                 if data is None:
-                    Player.MessageFrom("Kits", "Kit " + str(args[0]) + " not found!")
+                    Player.MessageFrom("Kits", "Kit " + kitname + " not found!")
                     return
                 if not data.Enabled:
                     Player.MessageFrom("Kits", "This kit has been disabled.")
@@ -259,29 +262,25 @@ class Kits:
                 if not data.NormalCanUse:
                     Player.MessageFrom("Kits", "You can't get this!")
                     return
-                CurrentUses = DataStore.Get("KitMaxUses" + args[0], Player.UID) or 0
+                CurrentUses = DataStore.Get("KitMaxUses" + kitname, Player.UID) or 0
                 if CurrentUses:
                     CurrentUses = int(CurrentUses)
-                if data.MaxUses > 0:
-                    if (Player.Admin and data.AdminCanBypassMaxUses) or\
-                            (Player.Moderator and data.ModeratorCanBypassMaxUses):
-                        CurrentUses = None
                 if data.Cooldown > 0:
                     cooldown = data.Cooldown / 60000 * 60
                     systick = TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds
-                    time = DataStore.Get("KitCooldown" + str(args[0]), Player.UID)
+                    time = DataStore.Get("KitCooldown" + kitname, Player.UID)
                     if time is None:
-                        DataStore.Add("KitCooldown" + str(args[0]), Player.UID, 7)
+                        DataStore.Add("KitCooldown" + kitname, Player.UID, 7)
                         time = 7
                     calc = systick - time
                     if calc >= cooldown or time == 7:
                         if data.ClearInvOnUse:
                             inv.Clear()
                         if data.MaxUses > 0 and CurrentUses:
-                            DataStore.Add("KitMaxUses" + args[0], Player.UID, CurrentUses + 1)
+                            DataStore.Add("KitMaxUses" + kitname, Player.UID, CurrentUses + 1)
                         self.GiveKit(data, inv)
-                        Player.MessageFrom("Kits", "Kit " + args[0] + " received!")
-                        DataStore.Add("KitCooldown" + str(args[0]), Player.UID,
+                        Player.MessageFrom("Kits", "Kit " + kitname + " received!")
+                        DataStore.Add("KitCooldown" + kitname, Player.UID,
                                       TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds)
                     else:
                         Player.MessageFrom("Kits", "You have to wait before using this again!")
@@ -293,15 +292,15 @@ class Kits:
                         if CurrentUses >= data.MaxUses:
                             Player.MessageFrom("Kits", "You have reached the maximum uses of this kit!")
                             return
-                        DataStore.Add("KitMaxUses" + args[0], Player.UID, CurrentUses + 1)
+                        DataStore.Add("KitMaxUses" + kitname, Player.UID, CurrentUses + 1)
                     self.GiveKit(data, inv)
-                    Player.MessageFrom("Kits", "Kit " + args[0] + " received!")
+                    Player.MessageFrom("Kits", "Kit " + kitname + " received!")
         elif cmd == "flushkit":
             if Player.Admin:
                 if len(args) == 0:
                     Player.MessageFrom("Kits", "/flushkit kitname")
                     return
-                text = str.join("", args)
+                text = str.join("", args).lower()
                 DataStore.Flush("KitCooldown" + text)
                 Player.MessageFrom("Kits", "Flushed all cooldowns for " + text + "!")
         elif cmd == "clearmaxuses":
@@ -312,7 +311,7 @@ class Kits:
                     Player.MessageFrom("Kits", "/clearmaxuses kitname steamid")
                     Player.MessageFrom("Kits", "/clearmaxuses kitname playername")
                     return
-                kitname = args[0]
+                kitname = args[0].lower()
                 value = args[1]
                 if value == "all":
                     DataStore.Flush("KitMaxUses" + kitname)
@@ -340,7 +339,7 @@ class Kits:
             if len(args) == 0 or len(args) > 1:
                 Player.MessageFrom("Kits", "Usage: /kituses kitname")
                 return
-            CurrentUses = DataStore.Get("KitMaxUses" + args[0], Player.UID)
+            CurrentUses = DataStore.Get("KitMaxUses" + args[0].lower(), Player.UID)
             if CurrentUses:
                 Player.MessageFrom("Kits", "Your current uses: " + str(CurrentUses))
             else:
