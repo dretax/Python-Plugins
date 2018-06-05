575e7f7e3317ad21dd27af0bfaa1d238d6b5574a
DreTaX <dretax@gmail.hu>
2018-06-05 10:53:41 +0200

    Some additions

--- FougeritePlugins/AntiSpam/AntiSpam.py
+++ FougeritePlugins/AntiSpam/AntiSpam.py
@@ -9,8 +9,10 @@ import System
 from System import *
 
 Cooldown = {}
+Detection = {}
 # Seconds that must pass after a player's message before he can send another one.
-FloodSeconds = 2
+FloodSeconds = 3
+
 
 class AntiSpam:
 
@@ -21,10 +23,20 @@ class AntiSpam:
             calc = TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds - Cooldown[Player.UID]
             if calc < FloodSeconds:
                 ChatEvent.NewText = "    "
-                Player.Message("WAIT A BIT BEFORE SENDING ANOTHER MESSAGE...")
+                Player.Message("[color red]PLEASE DON'T SPAM")
+                if Player.UID not in Detection.keys():
+                    Detection[Player.UID] = 0
+                Detection[Player.UID] = Detection[Player.UID] + 1
+                if Detection[Player.UID] == 3:
+                    Player.Disconnect()
+                    Server.BroadcastNotice(Player.Name + " Has been auto kicked for spamming")
             else:
                 Cooldown[Player.UID] = TimeSpan.FromTicks(DateTime.Now.Ticks).TotalSeconds
+                if Player.UID in Detection.keys():
+                    Detection.pop(Player.UID)
 
     def On_PlayerDisconnected(self, Player):
         if Player.UID in Cooldown.keys():
             Cooldown.pop(Player.UID)
+        if Player.UID in Detection.keys():
+            Detection.pop(Player.UID)
