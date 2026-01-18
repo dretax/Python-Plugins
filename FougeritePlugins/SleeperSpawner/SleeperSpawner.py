5a8424f1edd78dbff9928a585343856daad24cf4
DreTaX <dretax14@gmail.com>
2026-01-18 11:59:37 +0100

    SleeperSpawner

---
+++ FougeritePlugins/SleeperSpawner/SleeperSpawner.py
@@ -0,0 +1,29 @@
+﻿__author__ = 'DreTaX'
+__version__ = '1.0'
+__about__ = 'This plugin re-spawns all sleepers during server load.'
+
+import clr
+clr.AddReferenceByPartialName("Fougerite")
+import Fougerite
+
+"""
+    Class
+"""
+
+# Enable debug logging of failed sleeper spawns False/True
+# Sleeper spawn may fail if the player is dead, or not existent, etc.
+EnableSleeperSpawnLogging = False
+
+class SleeperSpawner:
+
+    """
+        Methods
+    """
+
+    def On_ServerLoaded(self):
+        sleeperSteamIds = PlayerCache.CachedPlayers.KeysCopy
+        for i in xrange(len(sleeperSteamIds)):
+            steamID = sleeperSteamIds[i]
+            success = World.ManuallySpawnSleeper(steamID)
+            if EnableSleeperSpawnLogging and not success:
+                Plugin.Log("FailedSleepers.txt", "Failed to spawn sleeper for SteamID: " + str(steamID))
